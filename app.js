e3f7d509fc9a62d75d7b445bd9319740b6c5ccfe
DhaivtV <vipatdhaivat25@gmail.com>
2023-10-12 21:26:57 +0530

    workflows added

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '1,000,00';
+
 
 app.get('/', (req, res) => {
     // set response content    
-        res.send(`<html>
-                    <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
-                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
-                            <img src="https://picsum.photos/400/400?random=1">
-                        </div>
-                    </body>
-                   </html>`);
+        res.send('Welcome to Aaikyam Backend Server');
  
-  console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
 
 })
 
